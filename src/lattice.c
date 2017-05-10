76b8aa4b02c228ffdc2795b3e40a0da328a5872d
German Caro <german.layer8@gmail.com>
2017-05-10 14:29:41 -0300

    Working makefile, print and fill

--- src/lattice.c
+++ src/lattice.c
@@ -1,8 +1,35 @@
 #include "lattice.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 int fill_lattice(int *lattice, int n, float p) {
+    float valor_aleatorio;
+    int i;
+    for(i=0; i<(n*n); i++){
+        valor_aleatorio = rand()%RAND_MAX;
+        if (valor_aleatorio < (p*RAND_MAX)){
+            lattice[i] = 1;
+
+        }
+        else {
+            lattice[i] = 0;
+        }
+    }
+
   return 0;
 }
 
 int print_lattice(int *lattice, int n) {
+    int i;
+    for(i=0;i<(n*n);i++) {
+        if((i%n)==0) {
+            printf("\n %i ", lattice[i]);
+        }
+        else {
+            printf(" %i ", lattice[i]);
+        }
+    }
+    printf("\n \n");
+
   return 0;
 }
