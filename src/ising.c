a7de631f36c76a6abdeb105cd0e9a2c84a7072c3
German Caro <german.layer8@gmail.com>
2017-05-11 18:22:19 -0300

    Metropolis funcionando

--- src/ising.c
+++ src/ising.c
@@ -1,20 +1,44 @@
 #include "stdlib.h"
 #include "time.h"
+#include <unistd.h>
+#include <stdio.h>
 
 #include "metropolis.h"
 #include "lattice.h"
 
 int main(int argc, char **argv) {
-  int n = 32;
-  int *lattice = malloc(n * n * sizeof(int));
-  float prob = 0.5;
-  float T = 2.0;
-  int niter = 2000;
-  srand(time(NULL));
-  fill_lattice(lattice, n, prob);
-  for (int i = 0; i < niter; i++) {
-    metropolis(lattice, n, T);
-  }
-  print_lattice(lattice, n);
-  return 0;
+	int c;
+	int n = 8;
+	float T = 10.0;
+	int niter = 2000;
+
+    while ((c = getopt (argc, argv, "n:t:i:h")) != -1)
+		switch (c)
+		  {
+		  case 'n':
+			n = atoi(optarg);
+			break;
+		  case 't':
+			T = atof(optarg);
+			break;
+		  case 'i':
+			niter = atoi(optarg);
+			break;
+		  case 'h':
+			  fprintf (stderr, "-n lado de la red\n"
+				"-t temperatura\n"
+				"-i numero de iteraciones\n");
+		  default:
+			break;
+      }
+
+	int *lattice = (int *)malloc(n * n * sizeof(int));
+	float prob = 0.5;
+	srand(time(NULL));
+	fill_lattice(lattice, n, prob);
+	for (int i = 0; i < niter; i++) {
+		metropolis(lattice, n, T);
+	}
+	print_lattice(lattice, n);
+	return 0;
 }
--- src/metropolis.c
+++ src/metropolis.c
@@ -1,13 +1,43 @@
 #include "metropolis.h"
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 int metropolis(int *lattice, int n, float T) {
+    int sitio;
+    sitio = pick_site(lattice, n);
+    flip(lattice, n, T, sitio);
   return 0;
 }
 
 int pick_site(int *lattice, int n) {
-  return 0;
+    return (int)round(((double)rand())/RAND_MAX * (n * n - 1));
 }
 
-int flip(int *lattice, int n, float T) {
+int flip(int *lattice, int n, float T, int sitio) {
+    int S, E, O, N;
+    E = (sitio%(n-1) == 0) ? sitio-n+1 : sitio+1;
+    if(E<0) E = 1;
+    S = (sitio>=n*(n-1)-1) ? sitio-(n*(n-1)) : sitio+n;
+    O = (sitio%(n)== 0) ? sitio+n-1 : sitio-1;
+    N = (sitio<(n-1)) ? sitio+(n*(n-1)) : sitio-n;
+//    printf("E: %i, S: %i, O: %i, N: %i, sitio: %i\n", E, S, O, N, sitio); 
+
+    float J = 1/T;
+    float aleatorio = (float)rand()/RAND_MAX;
+//    printf("Aleatorio: %f\n", aleatorio);
+    int energia_inicial = lattice[sitio]*(lattice[E]+lattice[N]+lattice[O]+lattice[S]);
+    lattice[sitio] *= -1;
+    int energia_final = lattice[sitio]*(lattice[E]+lattice[N]+lattice[O]+lattice[S]);
+    int delta_e = -J*(energia_final - energia_inicial);
+//    printf("Energia final: %i, Energia inicial: %i\n", energia_final, energia_inicial);
+
+    float pi = exp(-(1/T)*delta_e);
+    if (pi>1) { 
+        return 0;
+    }else{
+        if (aleatorio > pi) lattice[sitio] *= -1;
+    }
   return 0;
+
 }
