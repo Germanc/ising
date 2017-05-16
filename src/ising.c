c24b4f7eabf4bac0b34d077aaf8b654b2757d3de
German Caro <german.layer8@gmail.com>
2017-05-16 14:22:10 -0300

    Fix: dispersiones, added correlacion

--- src/ising.c
+++ src/ising.c
@@ -3,6 +3,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include "auxiliares.h"
+#include <math.h>
 
 #include "metropolis.h"
 #include "lattice.h"
@@ -55,6 +56,14 @@ int main(int argc, char **argv) {
 	int *lattice = (int *)malloc(n * n * sizeof(int));
 	srand(time(NULL));
 	fill_lattice(lattice, n, prob);
+	float* magneti;
+	float* energi;
+	float* rmagnet;
+	float* renerg;
+	magneti = (float *)malloc(sizeof(float)*niter);
+	energi = (float *)malloc(sizeof(float)*niter);
+	rmagnet = (float *)malloc(sizeof(float)*niter);
+	renerg = (float *) malloc(sizeof(float)*niter);
 	for ( int k = 0; k<npre; k++ ){
 		metropolis(lattice, n, T, B);
 	}
@@ -64,16 +73,45 @@ int main(int argc, char **argv) {
 		for (int j = 0; j < n*n; j++){
 			metropolis(lattice, n, T, B)/(n*n);
 		}
-		magnetizacion_ahora = magnetizacion(lattice, n)/(n*n*niter);
-		magnet = magnet + magnetizacion_ahora;
-		magnetsq = magnetsq + magnetizacion_ahora*magnetizacion_ahora;
-		energia_ahora = energia(lattice, n, T, B)/(n*n*niter);
-		energ = energ + energia_ahora;
-		energsq = energsq + energia_ahora*energia_ahora;
+		magnetizacion_ahora = magnetizacion(lattice, n)/(n*n);
+		energia_ahora = energia(lattice, n, T, B)/(n*n);
+
+		magnet = magnet + magnetizacion_ahora/niter;
+		magnetsq = magnetsq + magnetizacion_ahora*magnetizacion_ahora/niter;
+
+		energ = energ + energia_ahora/niter;
+		energsq = energsq + energia_ahora*energia_ahora/niter;
+		energi[i] = energia_ahora/niter;
+		magneti[i] = magnetizacion_ahora/niter;
 	}
-	float magnet_varianza = magnetsq - magnet*magnet;
-	float energia_varianza = energsq - energ*energ;
+	float sumam , sumae , sumamd=0, sumaed=0;
+	for (int i=0; i<niter;i++){
+		sumamd = sumamd + (magneti[i]-magnet)*(magneti[i]-magnet);
+		sumaed = sumaed + (energi[i]-energ)*(energi[i]-energ);
+	}
+	for (int k = 0; k<niter; k++){
+		sumam = 0;
+		sumae = 0;
+		for ( int i = 0; i<niter-k; i++){
+			sumam = sumam + (magneti[i]-magnet)*(magneti[i+k]-magnet);
+			sumae = sumae + (energi[i]-energ)*(energi[i+k]-energ);
+		}
+		rmagnet[k] = sumam/sumamd;
+		renerg[k] = sumae/sumaed;	
+	}
+	float magnet_varianza = sqrt(magnetsq - magnet*magnet);
+	float energia_varianza = sqrt(energsq - energ*energ);
     printf("%f %f %.15f %.15f %.15f %.15f\n", T, B, energ, energia_varianza, magnet, magnet_varianza);
 //	print_lattice(lattice, n);
+	FILE *correlacion;
+	correlacion = fopen("correlacion.dat", "w");
+
+	for(int i=0; i<niter; i++){
+		fprintf(correlacion, "%i %f %f\n", i, rmagnet[i], renerg[i]);
+	}
+
+	fclose(correlacion);
+	free(magneti);
+	free(energi);
 	return 0;
 }
