b8d507783e9ca20b2757623084dc9972d99d9979
German Caro <german.layer8@gmail.com>
2017-05-20 15:26:49 -0300

    Listo segundos vecinos

--- src/auxiliares.c
+++ src/auxiliares.c
@@ -1,14 +1,20 @@
+#include <stdio.h>
 float energia(int *lattice, int n, float T, float B, float J2){
     int i;
     float energ =0;
-    int S, E;
+    int S, E, SE, SW;
     float J = 1.0/T;
     J = 1;
     for(i=0;i<(n*n-1);i++){
         E = (i%(n-1) == 0) ? i-n+1 : i+1;
         if(E<0) E = 1;
         S = (i>=n*(n-1)-1) ? i-(n*(n-1))+1 : i+n;
-        energ += -J*lattice[i]*(lattice[E]+lattice[S])-B*lattice[i];
+        SW = (S%n) ? S+n-1: S-1;
+        SE = (S%(n-1)) ? S-n+1: S+1; 
+        if(SW<0) SW += n;
+        if(SE < 0) SE += n;
+
+        energ += -J*lattice[i]*(lattice[E]+lattice[S])-B*lattice[i]-J2*lattice[i]*(lattice[SE]+lattice[SW]);
     }
     return energ;
 }
--- src/metropolis.c
+++ src/metropolis.c
@@ -16,21 +16,31 @@ int pick_site(int *lattice, int n) {
 }
 
 int flip(int *lattice, int n, float T, int sitio, float B, float J2) {
-    int S, E, O, N;
+    int S, E, O, N, SW, SE, NW, NE;
     float J = 1.0;;
     E = (sitio%(n-1) == 0) ? sitio-n+1 : sitio+1;
     if(E<0) E = 1;
     S = (sitio>=n*(n-1)-1) ? sitio-(n*(n-1))+1 : sitio+n;
     O = (sitio%(n)== 0) ? sitio+n-1 : sitio-1;
-    N = (sitio<(n-1)) ? sitio+(n*(n-1)) : sitio-n;
+    N = (sitio<(n)) ? sitio+(n*(n-1)) : sitio-n;
+    SW = (S%n) ? S+n-1: S-1;
+    if(SW<0) SW += n;
+    SE = (S%(n-1)) ? S-n+1: S+1; 
+    if(SE < 0) SE += n;
+    NW = (N%n) ? N+n-1: N-1;
+    if(NW<0) NW+=n;
+    NE = (N%(n-1)) ? N-n+1: N+1; 
+    if(NE<0) NE+=n;
+//    printf("SE %i SW %i NW %i NE %i S %i N %i\n", SE, SW, NW, NW, S, N);
+
 //    printf("E: %i, S: %i, O: %i, N: %i, sitio: %i\n", E, S, O, N, sitio); 
 
     float aleatorio = (float)rand()/RAND_MAX;
 //    printf("Aleatorio: %f\n", aleatorio);
-    int energia_inicial = lattice[sitio]*(lattice[E]+lattice[N]+lattice[O]+lattice[S]);
+    int energia_inicial = -J*lattice[sitio]*(lattice[E]+lattice[N]+lattice[O]+lattice[S])-J2*lattice[sitio]*(lattice[SE]+lattice[NE]+lattice[SW]+lattice[NW]);
     lattice[sitio] *= -1;
-    int energia_final = lattice[sitio]*(lattice[E]+lattice[N]+lattice[O]+lattice[S]);
-    int delta_e = -J*(energia_final - energia_inicial) - B*(lattice[sitio]*(-1)-lattice[sitio]);;
+    int energia_final = -J*lattice[sitio]*(lattice[E]+lattice[N]+lattice[O]+lattice[S])-J2*lattice[sitio]*(lattice[SE]+lattice[NE]+lattice[SW]+lattice[NW]);
+    int delta_e = (energia_final - energia_inicial) - B*(lattice[sitio]*(-1)-lattice[sitio]);;
 //    printf("Energia final: %i, Energia inicial: %i\n", energia_final, energia_inicial);
 
     float pi = exp(-(1.0/T)*delta_e);
